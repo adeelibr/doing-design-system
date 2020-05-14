6ce5c050ad717322890158a3b1a28dae461c2743
adeelibr <adeelimranr@gmail.com>
2020-05-14 15:17:53 +0200

    update alerts

--- src/components/Alert/__stories__/Alert.stories.js
+++ src/components/Alert/__stories__/Alert.stories.js
@@ -17,21 +17,41 @@ export const Variant = () => (
   </React.Fragment>
 );
 
-export const WithTitle = () => (
-  <Alert onClose={action("clicked")}>
-    <AlertTitle>Alert</AlertTitle>
-    This is an alert — check it out!
-  </Alert>
-);
+export const WithTitle = () => {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <Alert open={open} setOpen={setOpen} onClose={action("clicked")}>
+      <AlertTitle>Alert</AlertTitle>
+      This is an alert — check it out!
+    </Alert>
+  );
+};
 
-export const WithAction = () => (
-  <Alert onClose={action("clicked")} action={<Button>Undo</Button>}>
-    This is an alert — check it out!
-  </Alert>
-);
+export const WithAction = () => {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <Alert
+      open={open}
+      setOpen={setOpen}
+      action={
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={(e) => {
+            setOpen(false);
+          }}
+        >
+          Accept
+        </Button>
+      }
+    >
+      This is an alert — check it out!
+    </Alert>
+  );
+};
 
 export const WithActionAndTitle = () => (
-  <Alert onClose={action("clicked")} action={<Button>Undo</Button>}>
+  <Alert action={<Button onClick={action("clicked")}>Undo</Button>}>
     <AlertTitle>Alert</AlertTitle>
     This is an alert — check it out!
   </Alert>
