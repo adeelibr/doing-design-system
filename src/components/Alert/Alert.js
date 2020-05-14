5348ca351d03cc6de8c0b7143422dd8a48c45d3b
Adeel Imran <adeelimranr@gmail.com>
2020-05-14 16:43:16 +0200

    update alerts props & story variation (#5)

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,11 +1,20 @@
 import React from "react";
 // Material-UI Components
+import { makeStyles } from "@material-ui/core/styles";
 import Collapse from "@material-ui/core/Collapse";
 import MuiAlert from "@material-ui/lab/Alert";
 
+const useStyles = makeStyles(() => {
+  return {
+    root: {
+      borderRadius: 0,
+    },
+  };
+});
+
 const Alert = React.forwardRef((props, ref) => {
-  const { onClose, ...other } = props;
-  const [open, setOpen] = React.useState(true);
+  const { open = true, setOpen = () => {}, onClose, ...other } = props;
+  const classes = useStyles();
   return (
     <Collapse in={open}>
       <MuiAlert
@@ -16,6 +25,9 @@ const Alert = React.forwardRef((props, ref) => {
             setOpen(false);
           },
         })}
+        classes={{
+          root: classes.root,
+        }}
         {...other}
         variant="filled"
       />
